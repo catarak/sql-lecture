d1fa61f2eeb9262c2b88713109fe388439bf8e89
catarak <ctarakajian@gmail.com>
2014-10-14 12:09:35 -0400

    more lecture stuff

--- database.rb
+++ database.rb
@@ -13,3 +13,27 @@ require 'sqlite3'
 # => [[3, "Godin"]]
 
 db = SQLite3::Database.new "blog-engine.db"
+
+puts "Name the author whose posts you'd like to read: "
+author_name = gets.strip
+author_query = """SELECT posts.title FROM posts 
+                  JOIN people on posts.people_id = people.id
+                  WHERE people.name = '#{author_name}'"""
+results = db.execute(author_query)
+# [["Posts Title"]]
+results.each do |result|
+  puts result[0]
+end
+
+#THIS IS NOT SAFE AGAINST SQL injects
+# author_name could equal '; DROP TABLE posts;
+# What you do instead is
+author_query = """SELECT posts.title FROM posts 
+                  JOIN people on posts.people_id = people.id
+                  WHERE people.name = ?"""
+results = db.execute(author_query, author_name)                 
+
+
+# def shift_a_person(db)
+#   db.execute("DELETE FROM people LIMIT 1")
+# end
